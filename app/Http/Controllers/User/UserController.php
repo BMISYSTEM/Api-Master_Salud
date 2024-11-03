0e1322644846eb885725e26e5ec0a0ba487739f3
bayron meneses <baironmenesesidarraga.990128@gmail.com>
2024-11-03 11:57:30 -0500

    se carga la funcionalidsad de eliminar y marcar atendida la citas

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -244,7 +244,7 @@ class UserController extends Controller
                 inner join motivos_consultas m on c.motivo = m.id
                 inner join horarios h on c.horario = h.id');
                 $resumenCitas = DB::select('select count(*) total,(select count(*) from citas where user = 7 and atendido = 1 ) atendidos,
-                (select count(*) from citas  atendido = 0) pendientes
+                (select count(*) from citas where  atendido = 0) pendientes
                 from citas ');
             }
             //code...
